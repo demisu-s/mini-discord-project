ffafe7d1c3528cd499aea1ade76e5779b125a2f8
demisu-s <damisusefu@gmail.com>
2024-12-31 01:13:56 -0800

    error fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client'
 
 import App from '@/App.jsx'
 import './index.css'
+//this change is for hot reload
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  
     <App />
   </React.StrictMode>,
 )
